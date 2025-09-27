3065b817b11c855559ff665a25128d2a52ebfd3a
Svend Aage Roperos Nielsen <svendroperos@outlook.com>
2025-09-27 21:59:55 +0200

    removed razorComponentRenderer

--- RazorComponentRenderer/ComponentParameters.cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace RazorComponentRenderer;
-
-// ReSharper disable once UnusedTypeParameter
-public readonly record struct ComponentParameters<TComponent>(IReadOnlyDictionary<string, object?> Parameters) where TComponent : IComponent;
